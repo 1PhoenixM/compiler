ffff5bb8bac4111a8220035bd3a87c8c9aaa5b8e
Melissa <phoenixemere@gmail.com>
2016-02-16 17:56:34 -0500

    Lexer almost complete, started parser.

--- compiler.js
+++ compiler.js
@@ -2,6 +2,7 @@
 document.getElementById('footer').style.color = "blue";
 //Keyword test
 var keywordTest = new RegExp("print|while|if");
+//Must be literal tests
 //Type test
 var typeTest = new RegExp("int|string|boolean");
 //Program test
@@ -52,104 +53,251 @@ var SymbolTableEntry = (function () {
     }
     return SymbolTableEntry;
 })();
-var testDFA = { 's1': { 'a': 's2', 'b': 's2', 'isAccepting': false }, 's2': { 'isAccepting': true } };
+var DFA = { 's1': { '+': 's2',
+        '"': 's3',
+        ' ': 's4',
+        '$': 's5',
+        '=': 's6',
+        '!': 's7',
+        '{': 's8',
+        '}': 's9',
+        'a': 's10',
+        'b': 's58',
+        'c': 's10',
+        'd': 's10',
+        'e': 's10',
+        'f': 's71',
+        'g': 's10',
+        'h': 's10',
+        'i': 's21',
+        'j': 's10',
+        'k': 's10',
+        'l': 's10',
+        'm': 's10',
+        'n': 's10',
+        'o': 's10',
+        'p': 's45',
+        'q': 's10',
+        'r': 's10',
+        's': 's65',
+        't': 's76',
+        'u': 's10',
+        'v': 's10',
+        'w': 's51',
+        'x': 's10',
+        'y': 's10',
+        'z': 's10',
+        '0': 's11',
+        '1': 's11',
+        '2': 's11',
+        '3': 's11',
+        '4': 's11',
+        '5': 's11',
+        '6': 's11',
+        '7': 's11',
+        '8': 's11',
+        '9': 's11',
+        '(': 's12',
+        ')': 's13'
+    },
+    's6': { '=': 's14', 'accept': 'T_assign' },
+    's7': { '=': 's15', 'accept': 'T_notEqualTo' },
+    's10': {
+        'accept': 'T_id' },
+    /*'s16' : {'i': 's23',
+             'u': 's24',},
+    's23' : {'n': 's25'},
+    's25' : {'t': 's26'},
+    's17' : {'l': 's27'},
+    's27' : {'s': 's28'},
+    's28' : {'e': 's29'},
+    's24' : {'e': 's30'},
+    's18' : {'t': 's31'},
+    's19' : {'o': 's32'},
+    's32' : {'l': 's33'},
+    's33' : {'e': 's34'},
+    's34' : {'a': 's35'},
+    's35' : {'n': 's36'},
+    's20' : {'r': 's37'},
+    's37' : {'i': 's38'},
+    's38' : {'n': 's39'},
+    's39' : {'g': 's40'},
+    's22' : {'i': 's41'},
+    's41' : {'l': 's42'},
+    's42' : {'e': 's43'},*/
+    //Accepting states
+    's2': { 'accept': 'T_intop' },
+    's3': { 'accept': 'T_quoteString' },
+    's4': { 'accept': 'T_space' },
+    's5': { 'accept': 'T_EOF' },
+    's8': { 'accept': 'T_openBlock' },
+    's9': { 'accept': 'T_closeBlock' },
+    's11': { 'accept': 'T_digit' },
+    's12': { 'accept': 'T_openArgList' },
+    's13': { 'accept': 'T_closeArgList' },
+    's14': { 'accept': 'T_testIfEqual' },
+    's15': { 'accept': 'T_notEqualTo' },
+    's21': { 'f': 's44', 'n': 's56', 'accept': 'T_id' },
+    's44': { 'accept': 'T_keywordIf' },
+    's45': { 'r': 's46', 'accept': 'T_id' },
+    's46': { 'i': 's47' },
+    's47': { 'n': 's48' },
+    's48': { 't': 's49' },
+    's49': { 'accept': 'T_keywordPrint' },
+    's51': { 'h': 's52', 'accept': 'T_id' },
+    's52': { 'i': 's53' },
+    's53': { 'l': 's54' },
+    's54': { 'e': 's55' },
+    's55': { 'accept': 'T_keywordWhile' },
+    's56': { 't': 's57' },
+    's57': { 'accept': 'T_typeInt' },
+    's58': { 'o': 's59', 'accept': 'T_id' },
+    's59': { 'o': 's60' },
+    's60': { 'l': 's61' },
+    's61': { 'e': 's62' },
+    's62': { 'a': 's63' },
+    's63': { 'n': 's64' },
+    's64': { 'accept': 'T_typeBoolean' },
+    's65': { 't': 's66', 'accept': 'T_id' },
+    's66': { 'r': 's67' },
+    's67': { 'i': 's68' },
+    's68': { 'n': 's69' },
+    's69': { 'g': 's70' },
+    's70': { 'accept': 'T_typeString' },
+    's71': { 'a': 's72', 'accept': 'T_id' },
+    's72': { 'l': 's73' },
+    's73': { 's': 's74' },
+    's74': { 'e': 's75' },
+    's75': { 'accept': 'T_boolFalse' },
+    's76': { 'r': 's77', 'accept': 'T_id' },
+    's77': { 'u': 's78' },
+    's78': { 'e': 's79' },
+    's79': { 'accept': 'T_boolTrue' } //true
+};
 //Step 1
 function lex(sourceCode) {
-    var tokens = regexT("", 0, "", sourceCode, []); /* issue: printf is recognized as print. throws away string when sees a valid keyword */
+    var tokens = getTokenStream(sourceCode); /* issue: printf is recognized as print. throws away string when sees a valid keyword */
     //console.log(sourceCode);
-    /*document.getElementById('machine-code').innerHTML = "";
-    for(var i = 0; i < tokens.length; i++){
-      document.getElementById('machine-code').innerHTML += tokens[i].tokenName + " is a " + tokens[i].tokenKind + " on line " + tokens[i].tokenLineNumber;
-    }*/
+    document.getElementById('machine-code').innerHTML = "";
+    for (var i = 0; i < tokens.length; i++) {
+        document.getElementById('machine-code').innerHTML += tokens[i].tokenName + " is a " + tokens[i].tokenKind + " on line " + tokens[i].tokenLineNumber + "<br />";
+    }
     //return tokens;
 }
-//Step 1 Helper
-/*function regexTest(charPointer: number, possibleLexeme: string, tokens: Token[]){
-  console.log(possibleLexeme.toString().substring(charPointer,possibleLexeme.length));
-  if(charPointer == possibleLexeme.length){
-     document.getElementById('machine-code').innerHTML = "";
-    for(var i = 0; i < tokens.length; i++){
-      document.getElementById('machine-code').innerHTML += tokens[i].tokenName + " is a " + tokens[i].tokenKind + " on line " + tokens[i].tokenLineNumber;
-     }
-     return tokens;
-  }
-  else if(keywordTest.test(possibleLexeme.toString().substring(charPointer,possibleLexeme.length))){
-    var t = new Token("keyword", possibleLexeme, 0);
-    tokens.push(t);
-    regexTest(charPointer+1, possibleLexeme, tokens);
-  }
-  else if(typeTest.test(possibleLexeme.toString().substring(charPointer,possibleLexeme.length))){
-    var t = new Token("type", possibleLexeme, 0);
-    tokens.push(t);
-    regexTest(charPointer+1, possibleLexeme, tokens);
-  }
-  else{
-    regexTest(charPointer+1, possibleLexeme, tokens);
-  }
-}
-*/
-//Keep running total of newline characters for line reporting
-function regexT(c, charPointer, currentLexeme, sourceCode, tokens) {
-    currentLexeme += c;
-    console.log(currentLexeme);
-    if (charPointer > sourceCode.length) {
-        /*if(keywordTest.test(currentLexeme)){
-            var t = new Token("keyword", currentLexeme, 0);
-            tokens.push(t);
+function getTokenStream(sourceCode) {
+    var state = 's1';
+    var tokens = [];
+    var currentString = "";
+    var lineNumber = 1;
+    var stringMode = false;
+    for (var i = 0; i < sourceCode.length; i++) {
+        var c = sourceCode.charAt(i);
+        var cnext = sourceCode.charAt(i + 1);
+        if (c == "\n") {
+            lineNumber++;
         }
-        else if(typeTest.test(currentLexeme)){
-            var t = new Token("type", currentLexeme, 0);
-            tokens.push(t);
-        }*/
-        document.getElementById('machine-code').innerHTML = "";
-        for (var i = 0; i < tokens.length; i++) {
-            document.getElementById('machine-code').innerHTML += tokens[i].tokenName + " is a " + tokens[i].tokenKind + " on line " + tokens[i].tokenLineNumber + "<br />";
-        }
-        ;
-        return tokens;
-    }
-    else {
-        var t = new Token("", currentLexeme, 0);
-        if (keywordTest.test(currentLexeme)) {
-            t.tokenKind = "keyword";
-        }
-        else if (typeTest.test(currentLexeme)) {
-            t.tokenKind = "type";
+        else if (c == " ") {
+            state = 's1';
+            currentString = "";
         }
         else {
-        }
-        if (t.tokenKind == "") {
-            //console.log(currentLexeme);
-            regexT(sourceCode.charAt(charPointer), charPointer + 1, currentLexeme, sourceCode, tokens);
-        }
-        else {
-            tokens.push(t);
-            regexT(sourceCode.charAt(charPointer), charPointer + 1, "", sourceCode, tokens);
+            state = DFA[state][c];
+            if ((DFA[state]['accept'] !== null && (DFA[state][cnext] === null) || cnext === '' || cnext === ' ')) {
+                //&& DFA[state][cnext] === null && DFA[state][cnext]['accept'] === null
+                currentString += c;
+                var t = new Token(DFA[state]['accept'], currentString, lineNumber);
+                if (t.tokenKind == "T_quoteString" && !stringMode) {
+                    stringMode = true;
+                }
+                else if (t.tokenKind == "T_quoteString" && stringMode) {
+                    stringMode = false;
+                }
+                tokens.push(t);
+                currentString = "";
+                state = 's1';
+            }
+            else {
+                currentString += c;
+            }
         }
     }
-    /*
-    var nextState = testDFA.s1[sourceCode.charAt(charPointer)];
-    if (testDFA.nextState.isAccepting) {var t = new Token("", "", 0)}
-    */
-    /*else if(keywordTest.test(currentLexeme)){
-        var t = new Token("keyword", currentLexeme, 0);
-        tokens.push(t);
-        regexT(sourceCode.charAt(charPointer), charPointer+1, "",
-        sourceCode, tokens);
-    }
-    else if(typeTest.test(currentLexeme)){
-        var t = new Token("type", currentLexeme, 0);
-        tokens.push(t);
-        regexT(sourceCode.charAt(charPointer), charPointer+1, "",
-        sourceCode, tokens);
-    }
-    else{
-        regexT(sourceCode.charAt(charPointer), charPointer+1, currentLexeme, sourceCode, tokens);
-    }*/
+    return tokens;
 }
 //Step 2 - Input: tokens, Output: CST
-function parse() {
+function parse(tokens) {
+    parseProgram(tokens);
 }
+function parseProgram(tokens) {
+    parseBlock(tokens.slice(0, length - 1));
+    //return tokens[tokens.length-1].tokenKind == "T_EOF";    
+}
+function parseBlock(tokens) {
+    parseStatementList(tokens.slice(1, length - 1));
+    //return tokens[0].tokenKind == "T_openBlock" && tokens[length-1].tokenKind == "T_closeBlock";
+}
+function parseStatementList(tokens) {
+    parseStatement(tokens);
+}
+function parseStatement(tokens) {
+    parsePrintStatement();
+    parseAssignmentStatement();
+    parseVarDeclStatement();
+    parseWhileStatement(tokens);
+    parseIfStatement(tokens);
+    parseBlock(tokens);
+}
+function parsePrintStatement() {
+    //T_print, T_openList, T_closeList
+    parseExpr();
+}
+function parseAssignmentStatement() {
+    //T_ID
+    //T_Assign
+    parseExpr();
+}
+function parseVarDeclStatement() {
+    //T_Type, T_ID
+}
+function parseWhileStatement(tokens) {
+    //T_while
+    parseBooleanExpr();
+    parseBlock(tokens);
+}
+function parseIfStatement(tokens) {
+    //T_if
+    parseBooleanExpr();
+    parseBlock(tokens);
+}
+function parseExpr() {
+    parseIntExpr();
+    parseStringExpr();
+    parseBooleanExpr();
+    //T_ID
+}
+function parseIntExpr() {
+    //T_Digit, T_Intop
+    parseExpr();
+    //or T_Digit
+}
+function parseStringExpr() {
+    //T_openString
+    parseCharList();
+    //T_closeString
+}
+function parseBooleanExpr() {
+    //T_openList
+    parseExpr();
+    //T_boolop
+    parseExpr();
+    //T_closeList
+    //or, T_boolval
+}
+function parseCharList() {
+    //empty
+    //T_char
+    //T_space
+}
+/**/
 //Step 3 - Input: CST, Output: AST
 function semanticAnalysis() {
 }
--- compiler.ts
+++ compiler.ts
@@ -78,7 +78,6 @@ interface TokenArray{
 	[index: number]: Token;
 }
 
-var testDFA = {'s1' : {'a': 's2', 'b': 's2', 'isAccepting': false }, 's2': {'isAccepting': true} };
 var DFA = {'s1' : {'+': 's2',
 		   '"': 's3',
 		   ' ': 's4',
@@ -86,30 +85,30 @@ var DFA = {'s1' : {'+': 's2',
 		   '=': 's6',
 		   '!': 's7',
 		   '{': 's8',
-		   '{': 's9',
+		   '}': 's9',
 		   'a': 's10',
-		   'b': 's10',
+		   'b': 's58',
 		   'c': 's10',
 		   'd': 's10',
 		   'e': 's10',
-		   'f': 's10',
+		   'f': 's71',
 		   'g': 's10',
 		   'h': 's10',
-		   'i': 's10',
+		   'i': 's21',
 		   'j': 's10',
 		   'k': 's10',
 		   'l': 's10',
 		   'm': 's10',
 		   'n': 's10',
 		   'o': 's10',
-		   'p': 's10',
+		   'p': 's45',
 		   'q': 's10',
 		   'r': 's10',
-		   's': 's10',
-		   't': 's10',
+		   's': 's65',
+		   't': 's76',
 		   'u': 's10',
 		   'v': 's10',
-		   'w': 's10',
+		   'w': 's51',
 		   'x': 's10',
 		   'y': 's10',
 		   'z': 's10',
@@ -125,18 +124,19 @@ var DFA = {'s1' : {'+': 's2',
 		   '9': 's11',
 		   '(': 's12',
 		   ')': 's13'
-		   }
-	    's6' : {'=': 's14'},
-	    's7' : {'=': 's15'},
-	    's10' : {'r': 's16',
+		   },
+	    's6' : {'=': 's14', 'accept': 'T_assign'},
+	    's7' : {'=': 's15', 'accept': 'T_notEqualTo'},
+	    's10' : {/*'r': 's16',
 	    	     'a': 's17',
 		     'n': 's18',
 		     'o': 's19',
 		     't': 's20',
-		     'f': 's21',
-		     'h': 's22'},
-	    's16' : {'i': 's23',
-	    	     'u': 's24'},
+		     //'f': 's21',
+		     'h': 's22',*/
+			 'accept': 'T_id'},
+	    /*'s16' : {'i': 's23',
+	    	     'u': 's24',},
 	    's23' : {'n': 's25'},
 	    's25' : {'t': 's26'},
 	    's17' : {'l': 's27'},
@@ -155,114 +155,119 @@ var DFA = {'s1' : {'+': 's2',
 	    's39' : {'g': 's40'},
 	    's22' : {'i': 's41'},
 	    's41' : {'l': 's42'},
-	    's42' : {'e': 's43'},
+	    's42' : {'e': 's43'},*/
 	    
 	    //Accepting states
-	    's2' : {'accept': 'intop'}
+	    's2' : {'accept': 'T_intop'},
+		's3' : {'accept': 'T_quoteString'},
+		's4' : {'accept': 'T_space'},
+		's5' : {'accept': 'T_EOF'},
+		's8' : {'accept': 'T_openBlock'},
+		's9' : {'accept': 'T_closeBlock'},
+		's11' : {'accept': 'T_digit'},
+		's12' : {'accept': 'T_openArgList'},
+		's13' : {'accept': 'T_closeArgList'},
+		's14' : {'accept': 'T_testIfEqual'},
+		's15' : {'accept': 'T_notEqualTo'},
+		's21' : {'f': 's44', 'n': 's56', 'accept': 'T_id'}, //if or int
+		
+		's44' : {'accept': 'T_keywordIf'},
+		
+		's45' : {'r': 's46', 'accept': 'T_id' }, //pr
+		's46' : {'i': 's47' }, //pri
+		's47' : {'n': 's48' }, //prin
+		's48' : {'t': 's49' }, //print
+		's49' : { 'accept': 'T_keywordPrint' }, //print
+		
+		's51' : {'h': 's52', 'accept': 'T_id' }, //wh
+		's52' : {'i': 's53' }, //whi
+		's53' : {'l': 's54' }, //whil
+		's54' : {'e': 's55' }, //while
+		's55' : { 'accept': 'T_keywordWhile' }, //while
+		
+		's56' : {'t': 's57' }, //in
+		's57' : { 'accept': 'T_typeInt' }, //int
+		
+		's58' : {'o': 's59', 'accept': 'T_id' }, //bo
+		's59' : {'o': 's60' }, //boo (ghost state)
+		's60' : {'l': 's61' }, //bool
+		's61' : {'e': 's62' }, //boole
+		's62' : {'a': 's63' }, //boolea
+		's63' : {'n': 's64' }, //boolean
+		's64' : { 'accept': 'T_typeBoolean' }, //boolean
+		
+		's65' : {'t': 's66', 'accept': 'T_id' }, //st
+		's66' : {'r': 's67' }, //str
+		's67' : {'i': 's68' }, //stri
+		's68' : {'n': 's69' }, //strin
+		's69' : {'g': 's70' }, //string
+		's70' : { 'accept': 'T_typeString' }, //string
+		
+		's71' : {'a': 's72', 'accept': 'T_id' }, //fa
+		's72' : {'l': 's73' }, //fal
+		's73' : {'s': 's74' }, //fals
+		's74' : {'e': 's75' }, //false
+		's75' : { 'accept': 'T_boolFalse' }, //false
+		
+		's76' : {'r': 's77', 'accept': 'T_id' }, //tr
+		's77' : {'u': 's78' }, //tru
+		's78' : {'e': 's79' }, //true
+		's79' : { 'accept': 'T_boolTrue' } //true
+		
+		/*'s26' : {'accept': 'T_keyword'}, //print
+		's29' : {'accept': 'T_keyword'}, //false
+		's30' : {'accept': 'T_keyword'}, //true
+		's43' : {'accept': 'T_keyword'}, //while*/
+		
 	    };
 
 //Step 1
 function lex(sourceCode: string){
-  var tokens = regexT("", 0, "", sourceCode, []); /* issue: printf is recognized as print. throws away string when sees a valid keyword */
+  var tokens = getTokenStream(sourceCode); /* issue: printf is recognized as print. throws away string when sees a valid keyword */
   //console.log(sourceCode);
-  /*document.getElementById('machine-code').innerHTML = "";
+  document.getElementById('machine-code').innerHTML = "";
   for(var i = 0; i < tokens.length; i++){
-    document.getElementById('machine-code').innerHTML += tokens[i].tokenName + " is a " + tokens[i].tokenKind + " on line " + tokens[i].tokenLineNumber;
-  }*/
+    document.getElementById('machine-code').innerHTML += tokens[i].tokenName + " is a " + tokens[i].tokenKind + " on line " + tokens[i].tokenLineNumber + "<br />";
+  }
   //return tokens;
 }
 
-//Step 1 Helper
-/*function regexTest(charPointer: number, possibleLexeme: string, tokens: Token[]){
-  console.log(possibleLexeme.toString().substring(charPointer,possibleLexeme.length));
-  if(charPointer == possibleLexeme.length){
-     document.getElementById('machine-code').innerHTML = "";
-  	for(var i = 0; i < tokens.length; i++){
-  	  document.getElementById('machine-code').innerHTML += tokens[i].tokenName + " is a " + tokens[i].tokenKind + " on line " + tokens[i].tokenLineNumber;
- 	 }
-     return tokens;
-  }
-  else if(keywordTest.test(possibleLexeme.toString().substring(charPointer,possibleLexeme.length))){
-    var t = new Token("keyword", possibleLexeme, 0);
-    tokens.push(t);
-    regexTest(charPointer+1, possibleLexeme, tokens);
-  }
-  else if(typeTest.test(possibleLexeme.toString().substring(charPointer,possibleLexeme.length))){
-    var t = new Token("type", possibleLexeme, 0);
-    tokens.push(t);
-    regexTest(charPointer+1, possibleLexeme, tokens);
-  }
-  else{
-    regexTest(charPointer+1, possibleLexeme, tokens);
-  }
-}
-*/
-//Keep running total of newline characters for line reporting
-function regexT(c: string, charPointer: number, currentLexeme: string, sourceCode: string, tokens: Token[]){
-	currentLexeme += c;
-	console.log(currentLexeme);
-	if(charPointer > sourceCode.length){
-		/*if(keywordTest.test(currentLexeme)){
-			var t = new Token("keyword", currentLexeme, 0);
-			tokens.push(t);
-		}
-		else if(typeTest.test(currentLexeme)){
-			var t = new Token("type", currentLexeme, 0);
-			tokens.push(t);
-		}*/
-        	document.getElementById('machine-code').innerHTML = "";
-  		for(var i = 0; i < tokens.length; i++){
-  		  document.getElementById('machine-code').innerHTML += tokens[i].tokenName + " is a " + tokens[i].tokenKind + " on line " + tokens[i].tokenLineNumber + "<br />"; };
-	     return tokens;
-	}
-	else{
-		var t = new Token("", currentLexeme, 0);
-		if(keywordTest.test(currentLexeme)){
-			t.tokenKind = "keyword";
-		}
-		else if(typeTest.test(currentLexeme)){
-			t.tokenKind = "type";
-		}
-		else{
-
-		}
-		if(t.tokenKind == ""){
-			//console.log(currentLexeme);
-			regexT(sourceCode.charAt(charPointer), charPointer+1, currentLexeme, sourceCode, tokens);	
-		}
-		else{
-			tokens.push(t);
-			regexT(sourceCode.charAt(charPointer), charPointer+1, "", sourceCode, tokens);	
-
-		}
-	}
-	/*
-	var nextState = testDFA.s1[sourceCode.charAt(charPointer)];
-	if (testDFA.nextState.isAccepting) {var t = new Token("", "", 0)}
-	if (testDFA.nextState.sourceCode.charAt(charPointer+1) !== null) {//no token yet. wait for longest match}
-	*/
-	/*
+function getTokenStream(sourceCode: String){
 	var state = 's1';
-        state = DFA.state.sourceCode.charAt(charPointer);
-	if(DFA.state.isAccepting) {var t = new Token(DFA.state.tokenKind, "", 0);}
-	if(DFA.state.sourceCode.charAt(charPointer+1) !== null) {//wait for longest match}
-	//Matrix here
-	*/
-	/*else if(keywordTest.test(currentLexeme)){
-		var t = new Token("keyword", currentLexeme, 0);
-		tokens.push(t);
-		regexT(sourceCode.charAt(charPointer), charPointer+1, "",
-		sourceCode, tokens);
+	var tokens = [];
+	var currentString = "";
+	var lineNumber = 1;
+	var stringMode = false;
+	for(var i = 0; i < sourceCode.length; i++){
+		var c = sourceCode.charAt(i);
+		var cnext = sourceCode.charAt(i+1);
+		if(c == "\n"){
+			lineNumber++;
+		}
+		else if(c == " "){
+			state = 's1';
+			currentString = "";
+		}
+		else{
+			state = DFA[state][c];
+			if((DFA[state]['accept'] !== null && (DFA[state][cnext] === null) || cnext === '' || cnext === ' ')) { //need to handle unknown chars gracefully
+			//&& DFA[state][cnext] === null && DFA[state][cnext]['accept'] === null
+				currentString += c;
+				var t = new Token(DFA[state]['accept'], currentString, lineNumber);
+				if(t.tokenKind == "T_quoteString" && !stringMode){
+					stringMode = true;
+				}
+				else if(t.tokenKind == "T_quoteString" && stringMode){
+					stringMode = false;
+				}
+				tokens.push(t);
+				currentString = "";
+				state = 's1';
+			}
+			else { currentString += c; }
+		}
 	}
-	else if(typeTest.test(currentLexeme)){
-		var t = new Token("type", currentLexeme, 0);
-		tokens.push(t);
-		regexT(sourceCode.charAt(charPointer), charPointer+1, "",
-		sourceCode, tokens);
-	}
-	else{
-		regexT(sourceCode.charAt(charPointer), charPointer+1, currentLexeme, sourceCode, tokens);
-	}*/
+	return tokens;
 }
 
 //Step 2 - Input: tokens, Output: CST
@@ -271,26 +276,26 @@ function parse(tokens: Token[]){
 }
 
 function parseProgram(tokens){
-  parseBlock(tokens[0..length-1]);
+  parseBlock(tokens.slice(0, length-1));
   //return tokens[tokens.length-1].tokenKind == "T_EOF";    
 }
 
-function parseBlock(){ //blocks inside blocks?
-  parseStatementList(tokens[1..length-1]);
+function parseBlock(tokens){ //blocks inside blocks?
+  parseStatementList(tokens.slice(1, length-1));
   //return tokens[0].tokenKind == "T_openBlock" && tokens[length-1].tokenKind == "T_closeBlock";
 }
 
-function parseStatementList(){
+function parseStatementList(tokens){
   parseStatement(tokens);
 }
 
-function parseStatement(){
+function parseStatement(tokens){
   parsePrintStatement();
   parseAssignmentStatement();
   parseVarDeclStatement();
-  parseWhileStatement();
-  parseIfStatement();
-  parseBlock();
+  parseWhileStatement(tokens);
+  parseIfStatement(tokens);
+  parseBlock(tokens);
 }
 
 function parsePrintStatement(){
@@ -308,16 +313,16 @@ function parseVarDeclStatement(){
  //T_Type, T_ID
 }
 
-function parseWhileStatement(){
+function parseWhileStatement(tokens){
   //T_while
   parseBooleanExpr();
-  parseBlock();
+  parseBlock(tokens);
 }
 
-function parseIfStatement(){
+function parseIfStatement(tokens){
   //T_if
   parseBooleanExpr();
-  parseBlock();
+  parseBlock(tokens);
 }
 
 function parseExpr(){
